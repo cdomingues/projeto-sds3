9630107041c1562fc49a14fec0ecdbcfea6deb61
Carlos Domingues <domingues_santos@hotmail.com>
2021-05-03 15:39:30 -0300

    Basic static components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,18 @@
+
+import NavBar from "components/NavBar";
+import Footer from "components/Footer";
+import DataTable from "components/DataTable";
 function App() {
   return (
-    <div>
-     <h1 className="text-primary">Teste ...</h1>
+    <>
+    <NavBar />
+    <div className="container">
+     <h1 className="text-primary">Good Morning World</h1>
+     <DataTable />
+    
     </div>
+    <Footer />
+    </>
   );
 }
 
